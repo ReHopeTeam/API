51357d30ed8c9cbdac4d007a594088558fb72019
MARIANA CORDEIRO DA SILVA <39318099860@info.local>
2026-06-11 10:59:33 -0300

    :construction: progress: inicializando a funcionalidade de logProduto

--- DTOs/LogProdutoDto/LerLogProdutoDto.cs
+++ DTOs/LogProdutoDto/LerLogProdutoDto.cs
@@ -2,6 +2,15 @@
 {
     public class LerLogProdutoDto
     {
+        public Guid LogProdutoID { get; set; }
+        public DateTime DataAnterior { get; set; }
+        public string NomeAnterior { get; set; } = string.Empty;
+        public decimal PrecoAnterior { get; set; }
+        public Boolean StatusProduto { get; set; }
 
+        public Guid ProdutoID { get; set; }
+        public int Codigo { get; set; }
+        public int LocalizacaoIDAnterior { get; set; }
+        public Guid UsuarioID { get; set; }
     }
 }
--- Interfaces/ILogProdutoRepository.cs
+++ Interfaces/ILogProdutoRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace ReHope.Interfaces
+﻿using ReHope.Domains;
+
+namespace ReHope.Interfaces
 {
     public interface ILogProdutoRepository
     {
+        List<LogProduto> Listar();
+        List<LogProduto> BuscarLogProdutoPorPodutoId(Guid produtoId);
+
     }
 }
