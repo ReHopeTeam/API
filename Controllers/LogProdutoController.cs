e4664ac9e601627b1d24cba14c982eb863776f25
MARIANA CORDEIRO DA SILVA <39318099860@info.local>
2026-06-11 10:59:33 -0300

    :construction: progress: inicializando a funcionalidade de logProduto

--- Controllers/LogProdutoController.cs
+++ Controllers/LogProdutoController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ReHope.Applications.Services;
+using ReHope.DTOs.LogProdutoDto;
+using ReHope.Exceptions;
 
 namespace ReHope.Controllers
 {
@@ -7,5 +11,35 @@ namespace ReHope.Controllers
     [ApiController]
     public class LogProdutoController : ControllerBase
     {
+        private readonly LogProdutoService _service;
+
+        public LogProdutoController(LogProdutoService service)
+        {
+            _service = service;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult<List<LerLogProdutoDto>> Listar()
+        {
+            List<LerLogProdutoDto> logProdutoDtos = _service.Listar();
+            return Ok(logProdutoDtos);
+        }
+
+        [Authorize]
+        [HttpGet("produto/{id}")]
+        public ActionResult<List<LerLogProdutoDto>> BuscarLogProdutoPorPodutoId(Guid produtoId)
+        {
+            try
+            {
+                List<LerLogProdutoDto> logProdutoDtos = _service.BuscarLogProdutoPorPodutoId(produtoId);
+                return Ok(logProdutoDtos);
+
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
--- Repository/LogProdutoRepository.cs
+++ Repository/LogProdutoRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace ReHope.Repository
+﻿using Microsoft.EntityFrameworkCore;
+using ReHope.Contexts;
+using ReHope.Domains;
+using ReHope.Interfaces;
+
+namespace ReHope.Repository
 {
-    public class LogProdutoRepository
+    public class LogProdutoRepository : ILogProdutoRepository
     {
+        private readonly ReHopeContext _context;
+        
+        public LogProdutoRepository(ReHopeContext context)
+        {
+            _context = context;
+        }
+
+        public List<LogProduto> Listar()
+        {
+            List<LogProduto> logProdutos = _context.LogProduto.OrderByDescending(logProduto => logProduto.DataAlteracao).ToList();
+
+            return logProdutos;
+        }
+        public List<LogProduto> BuscarLogProdutoPorPodutoId(Guid produtoId)
+        {
+            return _context.LogProduto
+                .Include(log => log.LogProdutoID)
+                .Include(log => log.NomeAnterior)
+                .Include(log => log.PrecoAnterior)
+                .Include(log => log.StatusProduto)
+                .Include(log => log.Codigo)
+                .Include(log => log.LocalizacaoIDAnterior)
+                .Include(log => log.UsuarioID)
+                .Where(log => log.ProdutoID == produtoId)
+                .OrderByDescending(log => log.DataAlteracao)
+                .ToList();
+        }
     }
 }
