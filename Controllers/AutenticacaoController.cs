457e50f403688f321ebab62b6d24d2a776599241
MARIANA CORDEIRO DA SILVA <39318099860@info.local>
2026-06-15 10:46:11 -0300

    :sparkles: feat: implementação da funcionalidade autenticação

--- Controllers/AutenticacaoController.cs
+++ Controllers/AutenticacaoController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ReHope.Applications.Services;
+using ReHope.DTOs.AutenticacaoDto;
+using ReHope.Exceptions;
 
 namespace ReHope.Controllers
 {
@@ -7,5 +10,26 @@ namespace ReHope.Controllers
     [ApiController]
     public class AutenticacaoController : ControllerBase
     {
+        private readonly AutenticacaoService _service;
+
+        public AutenticacaoController(AutenticacaoService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("login")]
+        public ActionResult<TokenDto> Login(LoginDto loginDto)
+        {
+            try
+            {
+                var token = _service.Login(loginDto);
+
+                return Ok(token);
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
--- DTOs/AutenticacaoDto/LoginDto.cs
+++ DTOs/AutenticacaoDto/LoginDto.cs
@@ -2,6 +2,7 @@
 {
     public class LoginDto
     {
-        
+        public string Email { get; set; } = null!;
+        public string Senha {  get; set; } = null!;
     }
 }
