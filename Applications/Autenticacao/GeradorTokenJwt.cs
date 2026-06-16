5f0efde812028c98787a31cad7595b442d77da95
MARIANA CORDEIRO DA SILVA <39318099860@info.local>
2026-06-16 08:00:23 -0300

    :test_tube: test: realização do teste de usuário e adição de StatusUsuario nos GETs de listar, listarPorID e listarPorEmail

--- Applications/Autenticacao/GeradorTokenJwt.cs
+++ Applications/Autenticacao/GeradorTokenJwt.cs
@@ -1,6 +1,57 @@
-﻿namespace ReHope.Applications.Autenticacao
+﻿using Microsoft.IdentityModel.Tokens;
+using ReHope.Exceptions;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ReHope.Applications.Autenticacao
 {
     public class GeradorTokenJwt
     {
+        private readonly IConfiguration _config;
+
+        public GeradorTokenJwt(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GerarToken(Token token)
+        {
+            var chave = _config["Jwt:Key"]!;
+
+            var issuer = _config["Jwt:Issuer"]!;
+
+            var audience = _config["Jwt:Audience"]!;
+
+            var keyBytes = Encoding.UTF8.GetBytes(chave);
+
+            if(keyBytes.Length < 32)
+            {
+                throw new DomainException("Jwt: key precisa ter pelo menos 32 caracteres"!);
+            }
+
+            var securityKey = new SymmetricSecurityKey(keyBytes);
+
+            var credencials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, usuario.UsuarioID.ToString()),
+
+                new Claim(ClaimTypes.Name, usuario.Nome),
+
+                new Claim(ClaimTypes.Email, usuario.Email)
+            };
+
+            var token = new JwtSecurityToken(
+
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                signingCredentials: credencials
+             );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
