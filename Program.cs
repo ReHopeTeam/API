fbe6d3f964cc42c3b124600a612754bb0510432a
CATARINA KONDOLIZA GARDENIA FAGUNDES DE MARTIN <42332992820@info.local>
2026-06-17 10:59:09 -0300

    :hammer: Ajustando a program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 using DotNetEnv;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using ReHope.Applications.Services;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using ReHope.Applications.Autenticacao;
 using ReHope.Applications.Services;
 using ReHope.Contexts;
 using ReHope.Interfaces;
 using ReHope.Repository;
+using ReHope.Interfaces;
+using ReHope.Repository;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,14 +24,91 @@ string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING"
 // conexao com o banco 
 builder.Services.AddDbContext<ReHopeContext>(options => options.UseSqlServer(connectionString));
 
-// Add services to the container.
-
-
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Value: Bearer TokenJWT"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+// Tipo Produto
+builder.Services.AddScoped<ITipoProdutoRepository, TipoProdutoRepository>();
+builder.Services.AddScoped<TipoProdutoService>();
+
+// Autentica��o 
+builder.Services.AddScoped<GeradorTokenJwt>();
+builder.Services.AddScoped<AutenticacaoService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
+    // Adiciona o suporte para autentica��o usando JWT.
+    .AddJwtBearer(options =>
+    {
+        // L� a chave secreta definida no appsettings.json.
+        var chave = Environment.GetEnvironmentVariable("JWT_KEY");
+        //var chave = builder.Configuration["Jwt:Key"]!;
+
+        // Quem emitiu o token.
+        var issuer = builder.Configuration["Jwt:Issuer"]!;
+
+        // Para quem o token foi criado.
+        var audience = builder.Configuration["Jwt:Audience"]!;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // Verifica se o emissor do token � v�lido.
+            ValidateIssuer = true,
+
+            // Verifica se o destinat�rio do token � v�lido.
+            ValidateAudience = true,
+
+            // Verifica se o token ainda est� v�lido.
+            ValidateLifetime = true,
+
+            // Verifica se a assinatura do token � v�lida.
+            ValidateIssuerSigningKey = true,
+
+            // Define qual emissor � considerado v�lido.
+            ValidIssuer = issuer,
+
+            // Define qual audience � considerado v�lido.
+            ValidAudience = audience,
+
+            // Define qual chave ser� usada para validar a assinatura do token.
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(chave)
+            ),
+
+            // o token geralmente tem 5 minutos de tolerancia, aqui colocamos para remover essa tolerancia
+            // remove toler�ncia extra no vencimento do token
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
@@ -34,12 +119,6 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod();
         });
 });
-
-
-// Tipo Produto
-builder.Services.AddScoped<ITipoProdutoRepository, TipoProdutoRepository>();
-builder.Services.AddScoped<TipoProdutoService>();
-
 
 var app = builder.Build();
 
